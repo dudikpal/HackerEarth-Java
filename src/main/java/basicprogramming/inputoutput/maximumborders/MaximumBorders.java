d0320b9ddb6e3587d4c84f6725e38f3b7eb37272
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-05-12 12:23:44 +0200

    #1 Basic Programming - Input/Output - Maximum Borders problem done

---
+++ src/main/java/basicprogramming/inputoutput/maximumborders/MaximumBorders.java
@@ -0,0 +1,95 @@
+package basicprogramming.inputoutput.maximumborders;
+
+import java.util.*;
+
+public class MaximumBorders {
+
+    public String solution(String input) {
+
+        Scanner scan = new Scanner(input);
+        int cases = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder();
+        String separator = "";
+        for (int i = 0; i < cases; i++) {
+            String[] rowsAndLength = scan.nextLine().split(" ");
+            int rows = Integer.parseInt(rowsAndLength[0]);
+            List<List<Integer>> shapes = new ArrayList<>();
+            int maxBorder = 0;
+            // read first & last indexes to array
+            for (int j = 0; j < rows; j++) {
+
+                String line = scan.nextLine();
+                int actualMax = 0;
+                int startIndex = line.indexOf("#");
+                int endIndex = line.lastIndexOf("#");
+                if (startIndex == -1) {
+                    if (j == 0 || shapes.size() == 0) {
+                        continue;
+                    }
+                    if ((actualMax = computeMAxBorderLength(shapes)) > maxBorder) {
+                        maxBorder = actualMax;
+                    }
+                    shapes.clear();
+                    continue;
+                }
+                if (shapes.size() != 0
+                        && (startIndex > shapes.get(shapes.size() - 1).get(1)
+                        || endIndex < shapes.get(shapes.size() - 1).get(0))) {
+                    if ((actualMax = computeMAxBorderLength(shapes)) > maxBorder) {
+                        maxBorder = actualMax;
+                    }
+                    shapes.clear();
+                }
+                shapes.add(List.of(startIndex, endIndex));
+                if (rows - 1 == j) {
+                    if ((actualMax = computeMAxBorderLength(shapes)) > maxBorder) {
+                        maxBorder = actualMax;
+                    }
+                }
+
+            }
+            sb.append(separator)
+                    .append(maxBorder);
+            separator = "\n";
+        }
+        return  sb.toString();
+    }
+
+
+    private int computeMAxBorderLength(List<List<Integer>> shapes) {
+        // first shape
+        int maxLength = Math.abs(shapes.get(0).get(0) - shapes.get(0).get(1)) + 1;
+        // horizontal shapes
+        for (int i = 1; i < shapes.size() - 1; i++) {
+            for (int j = 0; j < 2; j++) {
+                int shape = Math.abs(shapes.get(i).get(j) - shapes.get(i + 1).get(j));
+                if (shape > maxLength) {
+                    maxLength = shape;
+                }
+            }
+        }
+        // bottom shape
+        int bottomBorder = Math.abs(shapes.get(shapes.size() - 1).get(0) - shapes.get(shapes.size() - 1).get(1)) + 1;
+
+        if (bottomBorder > maxLength) {
+            maxLength = bottomBorder;
+        }
+        int leftShapeCounter = 1;
+        int rightShapeCounter = 1;
+        for (int i = 0; i < shapes.size() - 1; i++) {
+            if (shapes.get(i).get(0) == shapes.get(i + 1).get(0)) {
+                leftShapeCounter++;
+            } else {
+                leftShapeCounter = 1;
+            }
+            if (shapes.get(i).get(1) == shapes.get(i + 1).get(1)) {
+                rightShapeCounter++;
+            } else {
+                rightShapeCounter = 1;
+            }
+        }
+        leftShapeCounter = Math.max(rightShapeCounter, leftShapeCounter);
+
+        return Math.max(maxLength, leftShapeCounter);
+    }
+}
---
+++ src/test/java/basicprogramming/inputoutput/maximumborders/MaximumBordersTest.java
@@ -0,0 +1,220 @@
+package basicprogramming.inputoutput.maximumborders;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumBordersTest {
+
+    @Test
+    void testResult2() {
+        String input = """
+                1
+                7 5
+                .....
+                ..##.
+                ###..
+                ..##.
+                .....
+                ..#..
+                .#...""";
+        String expected = "2";
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+    @Test
+    void testWithEmptyLines() {
+        String input = """
+                1
+                14 2
+                ..
+                #.
+                ..
+                #.
+                ..
+                #.
+                ..
+                ..
+                #.
+                ..
+                ..
+                ..
+                #.
+                ..""";
+        String expected = "1";
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+    @Test
+    void test() {
+        String input = """
+                10
+                2 15
+                .....####......
+                .....#.........
+                7 9
+                ...###...
+                ...###...
+                ..#......
+                .####....
+                ..#......
+                ...#####.
+                .........
+                18 11
+                .#########.
+                ########...
+                .........#.
+                ####.......
+                .....#####.
+                .....##....
+                ....#####..
+                .....####..
+                ..###......
+                ......#....
+                ....#####..
+                ...####....
+                ##.........
+                #####......
+                ....#####..
+                ....##.....
+                .#######...
+                .#.........
+                1 15
+                .....######....
+                5 11
+                ..#####....
+                .#######...
+                ......#....
+                ....#####..
+                ...#####...
+                8 13
+                .....######..
+                ......##.....
+                ########.....
+                ...#.........
+                .............
+                #######......
+                ..######.....
+                ####.........
+                7 5
+                .....
+                ..##.
+                ###..
+                ..##.
+                .....
+                ..#..
+                .#...
+                14 2
+                ..
+                #.
+                ..
+                #.
+                ..
+                #.
+                ..
+                ..
+                #.
+                ..
+                ..
+                ..
+                #.
+                ..
+                7 15
+                .###########...
+                ##############.
+                ...####........
+                ...##########..
+                .......#.......
+                .....#########.
+                .#######.......
+                12 6
+                #####.
+                ###...
+                #.....
+                ##....
+                ###...
+                ......
+                .##...
+                ..##..
+                ...#..
+                ..#...
+                #####.
+                ####..""";
+
+        String expected = """
+                4
+                5
+                9
+                6
+                5
+                7
+                2
+                1
+                11
+                5""";
+
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+    @Test
+    void testFromFile() {
+        String input = "";
+        try {
+            input = Files.readString(Path.of("src/main/resources/basicprogramming/inputoutput/maximumborders/input.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String expected = """
+                508
+                448
+                718
+                326
+                489
+                741
+                407
+                22
+                183
+                330""";
+
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+    @Test
+    void testFromFile2() {
+        String input = "";
+        try {
+            input = Files.readString(Path.of("src/main/resources/basicprogramming/inputoutput/maximumborders/input2.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String expected = """
+                326""";
+
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+    @Test
+    void testFromFile3() {
+        String input = "";
+        try {
+            input = Files.readString(Path.of("src/main/resources/basicprogramming/inputoutput/maximumborders/input3.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String expected = """
+                326""";
+
+        assertEquals(expected, new MaximumBorders().solution(input));
+    }
+
+}
